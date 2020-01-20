8dcece17455480bb7634b02dcb8235a5fa32e8c4
Aaron <aarongillies88@gmail.com>
2020-01-20 18:45:58 -0500

    final changes to reach MVP

--- server.js
+++ server.js
@@ -34,6 +34,55 @@ server.get('/api/accounts/:id', (req, res) => {
     });
 });
 
+// POST - Add an account to the database
 
+server.post('/api/accounts', (req, res) => {
+    const accountData = req.body;
+    // const { id } = req.params.id
+    knex.insert(accountData, 'id')
+    .into('accounts')
+    .then(acc => {
+        res.status(201).json(acc);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            errorMessage: 'Error adding an account to the database'
+        })
+    });
+});
+
+//  PUT - Update an existing account by ID
+
+server.put('/api/accounts/:id', (req, res) => { // ALWAYS ADD WHERE ON UPDATES AND DELETES!
+    const { id } = req.params;
+    const updatedAccount = req.body;
+    knex('accounts').where({ id }).update(updatedAccount).then(count => {
+        res.status(200).json({ message: `${count} record(s) updated!`})
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            errorMessage: 'Error updating that account'
+        })
+    });
+});
+
+// DELETE - Delete an account by passing in a specific ID
+
+server.delete('/api/accounts/:id', (req, res) => {
+    knex('accounts')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            res.status(204).json({ message: `${count} record(s) deleted!`})
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({
+                errorMessage: 'Error removing that account!'
+            });
+        });
+});
 
 module.exports = server;
