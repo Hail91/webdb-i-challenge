61b6157108cd4042a8f37317bf93653e41510cc5
Aaron <aarongillies88@gmail.com>
2020-01-20 17:37:39 -0500

    Set up first 2 get endpoints for accounts data

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require('express');
 
-const db = require('./data/dbConfig.js');
+const knex = require('./data/dbConfig.js');
 
 const server = express();
 
 server.use(express.json());
+
+// Return a list of accounts from the database
+
+server.get('/api/accounts', (req, res) => {
+    knex.select('*').from('accounts').then(accounts => {
+        res.status(200).json(accounts);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            errorMessage: 'Error retrieving the accounts'
+        })
+    });
+});
+
+// GET -- Get a specific account from the database by ID
+
+server.get('/api/accounts/:id', (req, res) => {
+    knex.select('*').from('accounts').where({ id: req.params.id }).then(account => {
+        res.status(200).json(account);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            errorMessage: 'Error retrieving that account from the database'
+        })
+    });
+});
+
+
 
 module.exports = server;
